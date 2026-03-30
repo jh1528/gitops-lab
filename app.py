150c8910cbf259c1aaa4774f0d9a1bf00ecc788f
Jared Husson <jh1528@rit.edu>
2026-03-29 20:20:50 -0400

    On purpose to break the syntax error

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from GitOps-lab with Argo CD! (v2)"
+    return "Hello from GitOps-lab with Argo CD! # missing quote (on purpose)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
